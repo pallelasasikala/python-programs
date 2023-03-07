4d345c84816accd551198beaea25ca11b211c8f0
pallelasasikala <126751795+pallelasasikala@users.noreply.github.com>
2023-03-07 12:35:12 +0530

    Add files via upload

---
+++ task -1.py
@@ -0,0 +1,204 @@
+#type function
+'''a="sasikala"
+b=23
+c=34.6
+d={'s':43,'g':67}
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))'''
+
+#operators
+#Arithmetic Operations
+'''a=int(input('enter a number: '))
+b=int(input('enter a number: '))
+
+#Addition of two numbers
+add=a+b
+print('addition of two numbers is',add)
+print('addition of {0} and {1} is {2}'.format(a,b,add))
+
+#sub of two numbers
+sub=a-b
+print('substraction of two numbers is',sub)
+print('substraction of {0} and {1} is {2}'.format(a,b,sub))
+
+#product of two numbers
+product=a*b
+print('product of two numbers is',product)
+print('product of {0} and {1} is {2}'.format(a,b,product))
+
+#division operation
+div=a/b
+print('division is',div)'''
+
+
+
+'''a=float(input('enter a number: '))
+b=float(input('enter a number: '))
+#Addition of two numbers
+add=a+b
+print('addition of two numbers is',add)
+print('addition of {0} and {1} is {2}'.format(a,b,add))
+
+#sub of two numbers
+sub=a-b
+print('substraction of two numbers is',sub)
+print('substraction of {0} and {1} is {2}'.format(a,b,sub))
+
+#product of two numbers
+product=a*b
+print('product of two numbers is',product)
+print('product of {0} and {1} is {2}'.format(a,b,product))
+
+#division operation
+div=a/b
+print('division is',div)'''
+
+
+'''a = 3
+b = 5
+# a = a + b
+a += b
+print(a)'''
+
+# a = a - b
+'''a = 3
+b = 5
+a -= b
+print(a)'''
+
+# a = a * b
+'''a = 3
+b = 5
+a *= b
+print(a)'''
+
+# a = a / b
+'''a = 3
+b = 5
+a /= b
+print(a)'''
+
+# a = a % b
+'''a = 3
+b = 5
+a %= b
+print(a)'''
+
+# a = a // b
+'''a = 3
+b = 5
+a //= b
+print(a)'''
+
+# a = a ** b
+'''a = 3
+b = 5
+a **= b
+print(a)'''
+
+# a = a & b
+'''a = 3
+b = 5
+a &= b
+print(a)'''
+
+# a = a | b
+'''a = 3
+b = 5
+a |= b
+print(a)'''
+
+# a = a ^ b
+'''a = 3
+b = 5
+a ^= b
+print(a)'''
+
+# a = a >> b
+'''a = 3
+b = 5
+a >>= b
+print(a)'''
+
+# a = a << b
+'''a = 3
+b = 5
+a <<= b
+print(a)'''
+
+# Python program to illustrate the use of 'is' identity operator
+'''x = 5
+y = 5
+print(x is y)
+id(x)
+id(y)'''
+
+
+# Python program to illustrate the use of 'is' identity operator
+'''x=5
+y=4
+print(x is not y)
+id(x)
+id(y)'''
+
+#Membership operator
+'''l=[10,20,30,40,50]
+print(10 in l)
+
+print(60 not in l)'''
+
+#Relational operator
+'''a=5
+b=4
+#greater than operation
+print(a>b)
+
+#less than operation
+print(a<b)
+
+#greater than equal to operation
+print(a>=b)
+
+#less than or equal to operation
+print(a<=b)
+
+#equal to operation
+print(a==b)
+
+#not equal to operation
+print(a!=b)'''
+
+#Bitwise operator
+'''a=5
+b=3
+#Using right shift
+c=a>>2
+print(c)
+
+#using left shift
+d=a<<2
+print(d)
+
+#using and operation
+e=a & b
+print(e)
+
+#using or operator
+f=a | b
+print(f)
+
+#using xor operation
+g=a ^ b
+print(g)'''
+
+#operator precedence
+'''
+X = (5 + 3) * 2 ** 2
+print(X)
+
+
+'''
+
+print(10%2*4+5//5-3**2)
